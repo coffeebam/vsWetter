2d94d52184c42ef475a68f8f9fea70a2fec5437b
oelyaz <79798308+oelyaz@users.noreply.github.com>
2022-05-12 00:48:48 +0200

    flat laf funtkioniert -> multi window coden (aber jetzt: AB INS BETT!)

---
+++ src/main/java/MAIN.java
@@ -0,0 +1,13 @@
+import com.formdev.flatlaf.FlatDarculaLaf;
+
+import javax.swing.*;
+
+public class MAIN {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(new FlatDarculaLaf());
+        FRAME0 frame0 = new FRAME0();
+        //"com.sun.java.swing.plaf.windows.WindowsLookAndFeel"
+
+
+    }
+}
