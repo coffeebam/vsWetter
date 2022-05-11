238978406f236d548178cef4ca5d2c726186aaa5
oelyaz <79798308+oelyaz@users.noreply.github.com>
2022-05-12 00:48:48 +0200

    flat laf funtkioniert -> multi window coden (aber jetzt: AB INS BETT!)

--- src/main/java/FRAME0.java
+++ src/main/java/FRAME0.java
@@ -1,26 +1,46 @@
-import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatDarculaLaf;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FRAME0 extends JFrame{
 
-
-    private JTabbedPane tabbedPane1;
+//alle objekte in der UI werden deklariert
     private JPanel panel0;
-    private JButton button1;
+    private JToolBar bar;
+    private JButton leaderboardBtn;
+    private JButton overviewBtn;
+    private JPanel panel1;
 
     public FRAME0(){
+        //setzt anzeigefläche
         setContentPane(panel0);
-        setSize(500, 500);
-        setLocation(500, 1000);
         setTitle("vsWetter");
+        setLocation(1000, 500);
+        setSize(500, 500);
+
+        //programm endet wenn man das fenster schließt
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        ImageIcon mainIcon = new ImageIcon("src/main/resources/Blitz.png");
+        setIconImage(mainIcon.getImage());
+
+        //navigationsleist grauer hintergrund
+        bar.setBackground(Color.LIGHT_GRAY);
+        //knöpfe auf leiste lesbar
+        leaderboardBtn.setForeground(Color.BLACK);
+        overviewBtn.setForeground(Color.BLACK);
+
+        leaderboardBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
         setVisible(true);
-
     }
 
-    public static void main(String[] args) throws UnsupportedLookAndFeelException {
-        FRAME0 frame0 = new FRAME0();
-        UIManager.setLookAndFeel(new FlatDarkLaf());
-    }
+
 }
---
+++ src/main/java/FRAME1.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class FRAME1 extends JFrame {
+    private JPanel panel1;
+
+    public FRAME1(){
+        setContentPane(panel1);
+        setVisible(true);
+    }
+}
