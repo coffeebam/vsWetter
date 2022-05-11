8ef8d2d0acea5b6e11f7332bfb481fcb708758b9
oelyaz <79798308+oelyaz@users.noreply.github.com>
2022-05-11 13:56:53 +0200

    flatlaf funktioniert immernoch nicht

--- src/main/java/FRAME0.java
+++ src/main/java/FRAME0.java
@@ -1,2 +1,26 @@
-public class FRAME0 {
+import com.formdev.flatlaf.FlatDarkLaf;
+
+import javax.swing.*;
+
+public class FRAME0 extends JFrame{
+
+
+    private JTabbedPane tabbedPane1;
+    private JPanel panel0;
+    private JButton button1;
+
+    public FRAME0(){
+        setContentPane(panel0);
+        setSize(500, 500);
+        setLocation(500, 1000);
+        setTitle("vsWetter");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        FRAME0 frame0 = new FRAME0();
+        UIManager.setLookAndFeel(new FlatDarkLaf());
+    }
 }
