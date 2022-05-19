f3846a7a528b6edad71e361b21f7a0318ea8138e
oelyaz <79798308+oelyaz@users.noreply.github.com>
2022-05-19 23:35:24 +0200

    verschiedene pages funktionieren TO-DO: layout für überblick + wetter

--- src/main/java/FRAME0.java
+++ src/main/java/FRAME0.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,16 +7,28 @@ import java.awt.event.ActionListener;
 public class FRAME0 extends JFrame{
 
 //alle objekte in der UI werden deklariert
+    private static FRAME0 frame0;
     private JPanel panel0;
     private JToolBar bar;
     private JButton leaderboardBtn;
     private JButton overviewBtn;
     private JButton weatherBtn;
+    private JPanel page;
+    private JPanel überblick;
+    private JPanel leaderboard;
+    private JPanel wetter;
+    private JLabel first;
+    private JLabel second;
+    private JLabel third;
+    private JTable table;
+    private final Color grau = new Color(60, 63, 65);
 
-    public FRAME0(){
+    private FRAME0(){
         //setzt anzeigefläche
         setContentPane(panel0);
 
+        Dimension size = new Dimension(500, 500);
+        setSize(size);
         setLocation(1000, 500);
 
         //programm endet wenn man das fenster schließt
@@ -33,19 +46,66 @@ public class FRAME0 extends JFrame{
         overviewBtn.setForeground(Color.BLACK);
         weatherBtn.setForeground(Color.BLACK);
 
+        //startseite setzen
+        überblick.setVisible(true);
+        leaderboard.setVisible(false);
+        wetter.setVisible(false);
+
+        setVisible(true);
+        overviewBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                überblick.setVisible(true);
+                leaderboard.setVisible(false);
+                wetter.setVisible(false);
+            }
+        });
+
+        //alles für das leaderboard
         leaderboardBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                überblick.setVisible(false);
+                leaderboard.setVisible(true);
+                wetter.setVisible(false);
             }
         });
+        //erstellt Tabelle
+        //createUIComponents();
+        //table.setVisible(true);
 
-        setVisible(true);
+
+        weatherBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                überblick.setVisible(false);
+                leaderboard.setVisible(false);
+                wetter.setVisible(true);
+            }
+        });
     }
 
-    public JPanel getPanel0(){
-        return panel0;
+    public static FRAME0 getFrame0(){
+        if(frame0 == null){
+            frame0 = new FRAME0();
+        }
+        return frame0;
+    }
+
+    public void showLeaderboard(){
+        page.add(new JTable());
+
     }
 
 
+    private void createUIComponents() {
+        String[][] tableInhalt = {{"1.", "[bsp] openweather.com"}, {"2.", "[bsp] rapidweather.com"}};
+        String[] kopfzeile = {"Pos", "Anbieter"};
+        table = new JTable(tableInhalt, kopfzeile);
+        table.setBackground(grau);
+        table.setForeground(Color.LIGHT_GRAY);
+        table.removeEditor();
+
+    }
 }
--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -3,12 +3,11 @@ import com.formdev.flatlaf.FlatDarculaLaf;
 import javax.swing.*;
 
 public class MAIN {
-    public static void main(String[] args) throws UnsupportedLookAndFeelException, InterruptedException {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, InterruptedException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         UIManager.setLookAndFeel(new FlatDarculaLaf());
-        LAYOUT layout = new LAYOUT();
+        //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        FRAME0 frame0 = FRAME0.getFrame0();
         //"com.sun.java.swing.plaf.windows.WindowsLookAndFeel"
-
-        layout.nächsteCard();
 
     }
 }
